d6c5af9af2f08bebf1933ab3dcc16eaf14fc3c00
ThomasSaunders21 <thomasnz@icloud.com>
2024-09-08 15:05:07 +1200

    New shaders and asteroid spawner  system
    Updated  project so I can use  it  on Thomas's pc. Added shader to make the planets rim glow and a  basic asteroid  ring  system creator

---
+++ DIGITECHgame/Assets/Scripts/transformlistlist.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    [System.Serializable]
+    public class TRANSFORMS
+    {
+        public List<Transform> chunkpieces;
+    }
+    [System.Serializable]
+    public class TRANSFORMSLIST
+    {
+        public List<TRANSFORMS> chunklist;
+    }
+
